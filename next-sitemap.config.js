3fdb38145a5f8c33e5418eb20871c8e5dc0b03ce
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-05-03 05:36:17 +0900

    fix: regexp

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,5 +5,5 @@ module.exports = {
   generateRobotsTxt: true,
   sitemapSize: 7000,
   generateIndexSitemap: false,
-  exclude: ['\?tag=*', '/about', '\?category=*'],
+  exclude: ["\\?tag=*", "/about", "\\?category=*"],
 }
