8a9650df28a8091f4b2be59c54151ca88b7b6d4b
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-05-03 04:40:42 +0900

    fix: change to JP font
    - https://github.com/orioncactus/pretendard/releases/tag/v1.3.9

--- src/assets/fonts/pretendard/index.ts
+++ src/assets/fonts/pretendard/index.ts
@@ -3,47 +3,47 @@ import localFont from 'next/font/local'
 export const pretendard = localFont({
   src: [
     {
-      path: './Pretendard-Thin.woff2',
+      path: './PretendardJP-Thin.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: './Pretendard-ExtraLight.woff2',
+      path: './PretendardJP-ExtraLight.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: './Pretendard-Light.woff2',
+      path: './PretendardJP-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: './Pretendard-Regular.woff2',
+      path: './PretendardJP-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: './Pretendard-Medium.woff2',
+      path: './PretendardJP-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: './Pretendard-SemiBold.woff2',
+      path: './PretendardJP-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: './Pretendard-Bold.woff2',
+      path: './PretendardJP-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: './Pretendard-ExtraBold.woff2',
+      path: './PretendardJP-ExtraBold.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: './Pretendard-Black.woff2',
+      path: './PretendardJP-Black.woff2',
       weight: '900',
       style: 'normal',
     },
