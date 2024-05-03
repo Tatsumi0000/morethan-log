6c8db2a3e6bfe2b905497790b5ac2d7b03402477
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-05-03 17:10:11 +0900

    feat fix jp font subset

--- src/assets/fonts/pretendard/index.ts
+++ src/assets/fonts/pretendard/index.ts
@@ -1,51 +1,51 @@
-import localFont from 'next/font/local'
+import localFont from "next/font/local"
 
 export const pretendard = localFont({
   src: [
     {
-      path: './PretendardJP-Thin.woff2',
-      weight: '100',
-      style: 'normal',
+      path: "./PretendardJP-Thin.woff2",
+      weight: "100",
+      style: "normal",
     },
     {
-      path: './PretendardJP-ExtraLight.woff2',
-      weight: '200',
-      style: 'normal',
+      path: "./PretendardJP-ExtraLight.woff2",
+      weight: "200",
+      style: "normal",
     },
     {
-      path: './PretendardJP-Light.woff2',
-      weight: '300',
-      style: 'normal',
+      path: "./PretendardJP-Light.woff2",
+      weight: "300",
+      style: "normal",
     },
     {
-      path: './PretendardJP-Regular.woff2',
-      weight: '400',
-      style: 'normal',
+      path: "./PretendardJP-Regular.woff2",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: './PretendardJP-Medium.woff2',
-      weight: '500',
-      style: 'normal',
+      path: "./PretendardJP-Medium.woff2",
+      weight: "500",
+      style: "normal",
     },
     {
-      path: './PretendardJP-SemiBold.woff2',
-      weight: '600',
-      style: 'normal',
+      path: "./PretendardJP-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
     },
     {
-      path: './PretendardJP-Bold.woff2',
-      weight: '700',
-      style: 'normal',
+      path: "./PretendardJP-Bold.woff2",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: './PretendardJP-ExtraBold.woff2',
-      weight: '800',
-      style: 'normal',
+      path: "./PretendardJP-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
     },
     {
-      path: './PretendardJP-Black.woff2',
-      weight: '900',
-      style: 'normal',
+      path: "./PretendardJP-Black.woff2",
+      weight: "900",
+      style: "normal",
     },
   ],
 })
