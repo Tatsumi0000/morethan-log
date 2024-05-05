c909eb8efbdd0e78d1c26ef30b25a97c98134671
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-05-06 06:56:18 +0900

    delete sitemap output path

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,9 @@
 const { CONFIG } = require("./site.config")
 
 module.exports = {
-  siteUrl: CONFIG.link + "/",
+  siteUrl: CONFIG.link,
   generateRobotsTxt: true,
   sitemapSize: 7000,
   generateIndexSitemap: false,
   exclude: ["\\?tag=*", "/about", "\\?category=*", "/api*"],
-  outDir: ".vercel/output/static",
 }
