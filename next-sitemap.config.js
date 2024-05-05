ccba6b821a3f8656712ca58f64ffb6217e132976
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-05-06 06:07:10 +0900

    fix: siteURL prefix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 const { CONFIG } = require("./site.config")
 
 module.exports = {
-  siteUrl: CONFIG.link,
+  siteUrl: CONFIG.link + "/",
   generateRobotsTxt: true,
   sitemapSize: 7000,
   generateIndexSitemap: false,
